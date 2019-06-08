27584bfa71c6f8d224cc4cbd412435a0078c33e5
Juju-And <justyna.andrejczuk@gmail.com>
2019-06-08 23:00:23 +0000

    poprawki kosmetyczne

--- plot_weather.py
+++ plot_weather.py
@@ -3,36 +3,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# x_data = {}
-
 with open('data.json', 'r') as json_file:
     x_data = json.load(json_file)
 
-# print(data)
+regions = x_data['regions']
 
-one_region = x_data['regions']
-
-# print(one_region)
-for one in one_region:
+for one in regions:
 
     regions_name = one['name']
-    # # print(one_region)
-    # # print(one_region.__class__)
-    # # print(one_region.keys())
     weather_data = one['data']
-    # # print(weather_data.__class__)
-    # # print(weather_data[1])
+
     temp = []
     press = []
-    #
+
     for record in weather_data:
-        # print(record)
         temp.append(int(record['temperature']))
         press.append(int(record['pressure']))
-    # print(temp, press)
+
     df = pd.DataFrame({'temperature': temp, 'pressure': press})
     temp_standDev = round((np.std(temp)), 2)
-    # print(temp_standDev)
     press_standDev = round((np.std(press)), 2)
 
     plt.plot(temp, label='temperatura ({})'.format(temp_standDev))
@@ -44,6 +33,5 @@ for one in one_region:
     plt.title(regions_name)
     plt.tight_layout()
     plt.show()
-    plt.savefig(regions_name + '.pdf')
+    plt.savefig('results/{}.pdf'.format(regions_name))
     plt.clf()
-
